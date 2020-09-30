e28bb8c452076746c4ed7b4165f91bd4de8b9dc4
SamuelKelechi <kcfreshie@ymail.com>
2020-09-30 16:40:00 +0100

    number one commit

---
+++ script.js
@@ -0,0 +1,64 @@
+//Different ways of printing result
+// alert("Hello World")
+// document.write("This is Javascript Class")
+// console.log("JavaScript Class")
+// //TYPES OF DATATYPE
+//Primitive (Values that are unchanging) Data Type Are:
+
+//string
+//boolean
+//number: 
+//null: nothing
+//undefine: variable not assigned
+//symbol
+//Big Hint:
+
+//Non Primitive
+//object: just as in python, dictionary and list
+
+//string datatype
+//Assigning String Variables
+let fruits = "   orange, yam, pig"
+let sentences = "This is JavaScript Practice"
+let animal = "goat, sheep"
+
+console.log(fruits.length)
+console.log(fruits.toLowerCase())
+console.log(fruits.toUpperCase())
+//Trim is use to remove space
+console.log(fruits.trim)
+console.log(fruits.concat("sam").trim())
+//index of check for a character position
+console.log(sentences.indexOf("JavaScript"))
+let txt = "abcdefghijklm"
+console.log(txt.indexOf("h"))
+//to split character
+console.log(fruits.split("").toString())
+console.log(fruits.split(","))
+
+
+
+//number Datatype
+// let numb = 34;
+// console.log(typeof num)
+
+// let not;
+// console.log(typeof not)
+
+// let sym = symbol("samuel")
+// console.log(typeof sym)
+
+// let stu = {name:"sam", age:18}
+// console.log(typeof stu)
+
+// let student =["uche", 65, "ifeanyi"]
+// console.log(typeof student)
+
+// let isTrue = false;
+// console.log(typeof isTrue)
+
+// //Highest number that can integer can take
+
+// let number = (Number.MAX_SAFE_INTEGER)
+
+
